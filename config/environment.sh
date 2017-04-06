76484f2eb7bec7d9b7824953e0d70f1960ee7db2
Tiamo Laitakari <tiamo.laitakari@cs.helsinki.fi>
2017-04-06 15:28:56 +0300

    Fix.

--- config/environment.sh
+++ config/environment.sh
@@ -3,5 +3,5 @@
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="tilati"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tilati@users.cs.helsinki.fi/SpellBook"
+PROJECT_FOLDER="SpellBook"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
