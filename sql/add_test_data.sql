7d8bf1acf14b0a243d2b804b570ce84fce5a8498
Tiamo Laitakari <tiamo.laitakari@cs.helsinki.fi>
2017-04-06 18:42:25 +0300

    Bout viikko 2.

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,10 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Player (name, password) VALUES ('velho', 'salainen');
+INSERT INTO Player (name, password) VALUES ('noita', 'salainen');
+
+INSERT INTO Spell (name, type, school, level, components, castingtime, range, effect, targets, duration, savingthrow, spellresistance, description) VALUES('Magic Missile', 'Arcane', 'Evocation [Force]', 1, 'V, S', '1 Standard Action', 'Medium (100 ft. + 10 ft./level)', '', 'Up to five creatures, no two of which can be more than 15 ft. apart', 'Instantaneous', 'None', 'Yes', ' A missile of magical energy darts forth from your fingertip and strikes its target, dealing 1d4+1 points of force damage.
+
+The missile strikes unerringly, even if the target is in melee combat or has less than total cover or total concealment. Specific parts of a creature can’t be singled out. Inanimate objects are not damaged by the spell.
+
+For every two caster levels beyond 1st, you gain an additional missile—two at 3rd level, three at 5th, four at 7th, and the maximum of five missiles at 9th level or higher. If you shoot multiple missiles, you can have them strike a single creature or several creatures. A single missile can strike only one creature. You must designate targets before you check for spell resistance or roll damage.
+');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,34 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Player(
+       id SERIAL PRIMARY KEY,
+       name varchar(50) NOT NULL,
+       password varchar(50) NOT NULL
+       );
+
+CREATE TABLE Spellbook(
+       id SERIAL PRIMARY KEY,
+       player_id INTEGER REFERENCES Player(id),
+       name varchar(50) NOT NULL
+       );
+
+CREATE TABLE Spell(
+       id SERIAL PRIMARY KEY,
+       name varchar(50) NOT NULL,
+       type varchar(20) NOT NULL,
+       school varchar(20) NOT NULL,
+       level varchar(2) NOT NULL,
+       components varchar(20) NOT NULL,
+       castingtime varchar(40) NOT NULL,
+       range varchar(50) NOT NULL,
+       effect varchar(200),
+       targets varchar(200),
+       duration varchar(50) NOT NULL,
+       savingthrow varchar(20) NOT NULL,
+       spellresistance varchar(10) NOT NULL,
+       description varchar(1000) NOT NULL
+       );
+
+CREATE TABLE Spells(
+       spellbook_id INTEGER REFERENCES Spellbook(id),
+       spell_id INTEGER REFERENCES Spell(id)
+       );
