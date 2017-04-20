eef872f46d36da5eb12af3bbd108b2938087cda8
Tiamo Laitakari <tiamo.laitakari@cs.helsinki.fi>
2017-04-20 21:52:08 +0300

    Backend support for removing spells from spellbook.

--- app/models/spellbook.php
+++ app/models/spellbook.php
@@ -68,6 +68,11 @@ class SpellJoin extends BaseModel{
       $query->execute('spellbook_id'=> $this.spellbook_id, 'spell_id' => $this.spell_id); 
       }
 
+      public static function remove_from_spellbook(){
+      $query = DB::connection()->prepare('DELETE FROM Spells WHERE spellbook_id = :spellbook_id AND spell_id = :spell_id)');
+      $query->execute('spellbook_id'=> $this.spellbook_id, 'spell_id' => $this.spell_id); 
+      }
+
 }
 
 
