ee58e6e6f03f64c58b674065ced6e5b2231b40e0
shirotsune <tiamo.laitakari@cs.helsinki.fi>
2017-05-04 23:40:03 +0300

    debug 2

--- app/controllers/spellbook_controller.php
+++ app/controllers/spellbook_controller.php
@@ -76,6 +76,6 @@ class SpellbookController extends BaseController{
     public static function add_to_spellbook($id){
         $spells = Spell::all();
         $spellbook = Spellbook::find($id);
-        View::make('addtospellbook.html', array('spells' => $spells), array('spellbook' => $spellbook), $id);
+        View::make('addtospellbook.html', array('spellbook' => $spellbook), array('spells' => $spells), $id);
     }
 }
