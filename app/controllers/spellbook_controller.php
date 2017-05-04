583a806b68426936a996bbcbefe5b0b1eb0b4f10
shirotsune <tiamo.laitakari@cs.helsinki.fi>
2017-05-04 23:33:10 +0300

    Polishing

--- app/controllers/spellbook_controller.php
+++ app/controllers/spellbook_controller.php
@@ -75,7 +75,7 @@ class SpellbookController extends BaseController{
 
     public static function add_to_spellbook($id){
         $spells = Spell::all();
-        //$id
-        View::make('addtospellbook.html', array('spells' => $spells), $id);
+        $spellbook = Spellbook::find($id);
+        View::make('addtospellbook.html', array('spells' => $spells), array('spellbook' => $spellbook));
     }
 }
